04b2ba7f2b79a5553f2615fd54e07b6c033bd81f
Joel <contact@joelrodriguezaleman.com>
2017-08-29 20:45:31 +0200

    GildedRose refactor GenericQualityUpdater

--- src/Katas/GildedRose/PHP/GenericQualityUpdater.php
+++ src/Katas/GildedRose/PHP/GenericQualityUpdater.php
@@ -6,10 +6,10 @@ class GenericQualityUpdater implements QualityUpdaterInterface
 {
     public function updateQuality(Item $item)
     {
-        $item->sellIn = $item->sellIn - 1;
+        $item->sellIn--;
 
         $qualityDecrease = $item->sellIn < 0 ? 2 : 1;
-        $item->quality = $item->quality - $qualityDecrease;
+        $item->quality -= $qualityDecrease;
 
         if ($item->quality < 0) {
             $item->quality = 0;
