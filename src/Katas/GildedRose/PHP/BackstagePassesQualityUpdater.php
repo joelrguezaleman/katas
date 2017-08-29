f21165bf8689e5858b024fb2d038b47f44fa7461
Joel <contact@joelrodriguezaleman.com>
2017-08-29 20:44:11 +0200

    GildedRose refactor BackstagePassesQualityUpdater

--- src/Katas/GildedRose/PHP/BackstagePassesQualityUpdater.php
+++ src/Katas/GildedRose/PHP/BackstagePassesQualityUpdater.php
@@ -6,23 +6,26 @@ class BackstagePassesQualityUpdater implements QualityUpdaterInterface
 {
     public function updateQuality(Item $item)
     {
-        $item->quality = $item->quality + 1;
+        $this->increaseQuality($item);
         
         if ($item->sellIn < 11) {
-            if ($item->quality < 50) {
-                $item->quality = $item->quality + 1;
-            }
+            $this->increaseQuality($item);
         }
         if ($item->sellIn < 6) {
-            if ($item->quality < 50) {
-                $item->quality = $item->quality + 1;
-            }
+            $this->increaseQuality($item);
         }
 
-        $item->sellIn = $item->sellIn - 1;
+        $item->sellIn--;
 
         if ($item->sellIn < 0) {
-            $item->quality = $item->quality - $item->quality;
+            $item->quality = 0;
+        }
+    }
+
+    private function increaseQuality(Item $item)
+    {
+        if ($item->quality < 50) {
+            $item->quality++;
         }
     }
 }
